aa8a15dc23157d601cc40904ac91ef6f022be2a0
Mic <kittiphong.mic@gmail.com>
2015-10-14 22:21:53 +0700

    overflow tbody

--- src/tr.multiple.select.js
+++ src/tr.multiple.select.js
@@ -33,15 +33,18 @@
 
 		activeMouse: function() {
 			var that = this;
-			$(this.el).find('tbody tr').on('click', function(e) {
+			
+			$(this.el).parents(document).on('click', 'tbody tr', function(e) {
 				var tmsSelected = $(this).find('[data-tms-selected]');
 				var optionValue = $(this).find('td').data('tms-value');
 				if(tmsSelected.hasClass('tms-show')) {
+					// hide
 					tmsSelected.removeClass('tms-show');
 					tmsSelected.css({ color: '#ddd' }).html(that.opts.symbolCode);
 					that.deselect(optionValue);
 				}
 				else {
+					// show
 					tmsSelected.addClass('tms-show');
 					tmsSelected.css({ color: 'inherit' }).html(that.opts.symbolCode);
 					that.select(optionValue);
@@ -73,10 +76,42 @@
 		},
 
 		setLimit: function() {
-			var limit = this.opts.limit;
-			var rowHeight = $(this.el).find('tbody tr').outerHeight();
-			var overflowHeight = rowHeight * 5;
-			$(this.el).find('tbody').css({ 'height': overflowHeight, 'display': 'inline-block', 'overflow-y': 'auto', 'width': '100%', 'clear': 'both' });
+			var that = this;
+			var divContainer = $('<div/>', { 'class': 'tms-container' });
+			var divHeader = $('<div/>', { 'class': 'tms-header' });
+			var divBody = $('<div/>', { 'class': 'tms-body' });
+			var table = '<table class="'+this.el.className+'">%s%</table>';
+			
+			// set header
+			if($(this.el).find('thead').length > 0) {
+				var tableHeader = table.replace('%s%', $(this.el).find('thead')[0].outerHTML);
+				$(divHeader).append(tableHeader);
+				$(divContainer).append(divHeader);
+			}
+			
+			// set body
+			var tableBody = table.replace('%s%', $(this.el).find('tbody')[0].outerHTML);
+			$(divBody).append(tableBody);
+			$(divContainer).append(divBody);
+
+			// replace div container to existing table
+			$(this.el).replaceWith(divContainer);
+
+			// set width
+			var widthArray = [];
+			$(divContainer).find('.tms-body tbody tr:first td').each(function(index, td) {
+				var tdWidth = $(td).width();
+				widthArray.push(tdWidth);
+			});
+			
+			$(divContainer).find('.tms-header table thead th').each(function(i, th) {
+				$(th).width(widthArray[i])
+			});
+
+			// set height by limit
+			var trHeight = $(divContainer).find('.tms-body tbody tr').outerHeight()*this.opts.limit;
+			$(divContainer).find('.tms-header table').css({ 'margin': 0 });
+			$(divContainer).find('.tms-body').css({ 'height': trHeight, 'overflow-y': 'auto' })
 		}
 	}
 	
